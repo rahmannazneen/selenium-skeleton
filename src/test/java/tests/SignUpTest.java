e5f6d43340f7e6528a9c5e247c28e0d007d93328
nazneen.rahman <itsnazn33n@gmail.com>
2025-03-25 13:02:01 +0600

    Add the sign-up feature.

--- src/test/java/tests/SignUpTest.java
+++ src/test/java/tests/SignUpTest.java
@@ -2,12 +2,18 @@ package tests;
 
 import com.automation.selenium.pages.SignUpPage;
 import org.testng.annotations.Test;
+import tests.utils.Constant;
 
-public class SignUpTest extends BaseTest{
+public class SignUpTest extends BaseTest {
     @Test
-    public void testValidSignUp(){
+    public void testValidSignUp() throws InterruptedException {
         SignUpPage signUpPage = new SignUpPage(webDriver);
         signUpPage.clickSignInLink();
+        Thread.sleep(3000);
+        signUpPage.insertUsername(Constant.signup_username);
+        signUpPage.insertPassword(Constant.signup_password);
+        signUpPage.clickSubmitButton();
+        Thread.sleep(3000);
 
 
     }
